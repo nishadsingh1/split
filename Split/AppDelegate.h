3f976029bd7f4324d83edb60e008f2f2be580de7
Nishad Singh <nishadsingh@berkeley.edu>
2015-02-07 14:41:34 -0800

    Initial Commit

---
+++ Split/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Split
+//
+//  Created by Nishad Singh on 2/7/15.
+//  Copyright (c) 2015 Nishad Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Split/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Split
+//
+//  Created by Nishad Singh on 2/7/15.
+//  Copyright (c) 2015 Nishad Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
